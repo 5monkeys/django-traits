c385ed97b171788251a465ca302adeeb2a3b12a8
Anton Agestam <git@antonagestam.se>
2021-10-07 17:49:40 +0200

    Various fixes
    - Fixes overload of `Trait.__get__` and add type tests that locks this
      in.
    - Drops `Trait.as_q()` in favor of a simpler `Trait.q` abstract
      property.
    - Improves error messages for misconfiguration and adds tests.

--- tests/test_trait.py
+++ tests/test_trait.py
@@ -1,4 +1,7 @@
 import pytest
+from django.db import models
+
+from traits import Trait
 
 from .app.models import Person
 
@@ -11,3 +14,48 @@ def test_basics() -> None:
     assert list(query_rich) == [rich]
     query_poor = Person.objects.filter(~Person.is_rich.q)
     assert list(query_poor) == [poor]
+
+
+def test_instantiating_abstract_traits_raises_type_error() -> None:
+    class MissingCheckInstance(Trait):
+        q = models.Q()
+
+    class MissingQ(Trait):
+        def check_instance(self, instance: models.Model) -> bool:
+            return True
+
+    raises = pytest.raises(TypeError, match=r"^Can't instantiate")
+
+    with raises:
+        MissingCheckInstance()  # type: ignore[abstract]
+    with raises:
+        MissingQ()  # type: ignore[abstract]
+
+
+class DummyTrait(Trait):
+    q = models.Q()
+
+    def check_instance(self, instance: models.Model) -> bool:
+        return True
+
+
+def test_reusing_bound_trait_raises_type_error() -> None:
+    class ConflictingOwner:
+        trait = DummyTrait()
+
+    with pytest.raises(RuntimeError) as exc_info:
+
+        class _:
+            trait = ConflictingOwner.trait
+
+    assert type(exc_info.value.__cause__) is TypeError
+    assert str(exc_info.value.__cause__) == "Can't reuse bound trait"
+
+
+def test_filtering_unbound_trait_raises_type_error() -> None:
+    unbound = DummyTrait()
+    with pytest.raises(
+        TypeError,
+        match=fr"Can't call \.all\(\) on unbound {DummyTrait.__qualname__}",
+    ):
+        unbound.all()
